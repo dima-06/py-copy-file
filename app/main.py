e7a29cd505112cec62064c7019bb2a6109bddfe7
Dmytro Hrabovskyi <dima.grabovskiy2000@gmail.com>
2025-04-08 23:09:44 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    names_of_files = command.strip().split(" ")
+    if len(names_of_files) != 3 or names_of_files[0] != "cp":
+        return
+    file, new_file = names_of_files[1], names_of_files[2]
+    if not os.path.exists(file):
+        return
+    if file != new_file:
+        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
