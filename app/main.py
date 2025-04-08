6f045feae9ce6b8b3a8f11060921fb6e5d530330
Dmytro Hrabovskyi <dima.grabovskiy2000@gmail.com>
2025-04-08 23:13:25 +0300

    changed file -> source_file and new_file -> destination_file

--- app/main.py
+++ app/main.py
@@ -5,9 +5,9 @@ def copy_file(command: str) -> None:
     names_of_files = command.strip().split(" ")
     if len(names_of_files) != 3 or names_of_files[0] != "cp":
         return
-    file, new_file = names_of_files[1], names_of_files[2]
-    if not os.path.exists(file):
+    source_file, destination_file = names_of_files[1], names_of_files[2]
+    if not os.path.exists(source_file):
         return
-    if file != new_file:
-        with open(file, "r") as file_in, open(new_file, "w") as file_out:
+    if source_file != destination_file:
+        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
             file_out.write(file_in.read())
